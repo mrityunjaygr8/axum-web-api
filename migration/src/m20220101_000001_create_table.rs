a16d5c5687cb79a591ce16a6abb26b2626f13c14
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2026-02-19 15:57:07 +0530

    todo removed basic post table is being created and removed

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -7,7 +7,6 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
             .create_table(
@@ -24,7 +23,6 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
             .drop_table(Table::drop().table(Post::Table).to_owned())
